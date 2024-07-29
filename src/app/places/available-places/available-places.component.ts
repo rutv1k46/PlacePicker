8d1f34fd18ecc1cf6bab1b693b53029eb1e991a7
rutv1k46 <rutviksolanki01@gmail.com>
2024-07-29 13:59:54 -0400

    get available places

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,16 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from "@angular/core";
 
-import { Place } from '../place.model';
-import { PlacesComponent } from '../places.component';
-import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { Place } from "../place.model";
+import { PlacesComponent } from "../places.component";
+import { PlacesContainerComponent } from "../places-container/places-container.component";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-available-places',
-  standalone: true,
-  templateUrl: './available-places.component.html',
-  styleUrl: './available-places.component.css',
-  imports: [PlacesComponent, PlacesContainerComponent],
+    selector: "app-available-places",
+    standalone: true,
+    templateUrl: "./available-places.component.html",
+    styleUrl: "./available-places.component.css",
+    imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
-  places = signal<Place[] | undefined>(undefined);
+export class AvailablePlacesComponent implements OnInit {
+    places = signal<Place[] | undefined>(undefined);
+    private httpClient = inject(HttpClient);
+    private destroyRef = inject(DestroyRef);
+
+    ngOnInit() {
+        const subscription = this.httpClient
+            .get<{places: Place[]}>("http://localhost:3000/places")
+            .subscribe((resData) => {
+                console.log(resData);
+            });
+
+        this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    }
 }
